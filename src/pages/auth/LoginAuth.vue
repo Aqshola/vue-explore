91e894735e4e0cce3513529fb405f45b60b94327
aqshola <aqsholafid2@gmail.com>
2022-12-27 16:50:59 +0700

    feat: add login and register page

--- src/pages/auth/LoginAuth.vue
+++ src/pages/auth/LoginAuth.vue
@@ -1,5 +1,79 @@
 <template>
   <q-page-container>
-    <q-page> Login </q-page>
+    <q-page>
+      <div class="row" style="height: 100vh">
+        <div class="col-md-6 gt-sm bg-red-7 q-pa-xl" style="height: 100%">
+          <h2 class="text-h3 text-white">VueDo</h2>
+          <p class="text-h5 text-white">Make Your Scheduling Task Easier</p>
+        </div>
+        <div class="col-md-6 col-12 q-pa-xl">
+          <h1 class="text-h3 text-center text-weight-bold">Login</h1>
+          <q-form
+            class="q-gutter-md q-mt-xl"
+            v-bind:style="
+              ($q.screen.md || $q.screen.gt.md) && {
+                width: '500px',
+                margin: 'auto',
+              }
+            "
+          >
+            <q-input outlined v-model="email" label="Email" lazy-rules />
+
+            <div>
+              <q-input
+                type="password"
+                outlined
+                v-model="password"
+                label="Password"
+                lazy-rules
+                class="q-mb-sm"
+              />
+              <router-link
+                to="/"
+                style="text-decoration: none"
+                class="text-subtitle-2 text-blue-7"
+                >Forgot Password</router-link
+              >
+            </div>
+
+            <div>
+              <q-btn
+                glossy
+                label="Login"
+                type="submit"
+                color="blue-7"
+                style="width: 100%"
+              />
+            </div>
+            <div class="q-mt-md">
+              <span> Didn't have ccount ? </span>
+              <router-link
+                to="/register"
+                style="text-decoration: none"
+                class="text-weight-medium text-blue-7"
+                >Register</router-link
+              >
+            </div>
+          </q-form>
+        </div>
+      </div>
+    </q-page>
   </q-page-container>
 </template>
+
+<script lang="ts">
+import { useQuasar } from 'quasar';
+
+export default {
+  setup() {
+    const $q = useQuasar();
+    const email = '';
+    const password = '';
+    return {
+      email,
+      password,
+      $q,
+    };
+  },
+};
+</script>
--- src/pages/auth/RegisterAuth.vue
+++ src/pages/auth/RegisterAuth.vue
@@ -1,5 +1,81 @@
 <template>
   <q-page-container>
-    <q-page> Register</q-page>
+    <q-page>
+      <div class="row" style="height: 100vh">
+        <div class="col-md-6 gt-sm bg-red-7 q-pa-xl" style="height: 100%">
+          <h2 class="text-h3 text-white">VueDo</h2>
+          <p class="text-h5 text-white">
+            Join Us And Make Your Scheduling Task Easier
+          </p>
+        </div>
+        <div class="col-md-6 col-12 q-pa-xl">
+          <h1 class="text-h3 text-center text-weight-bold">Register</h1>
+          <q-form
+            class="q-gutter-md q-mt-xl"
+            v-bind:style="
+              ($q.screen.md || $q.screen.gt.md) && {
+                width: '500px',
+                margin: 'auto',
+              }
+            "
+          >
+            <q-input outlined v-model="name" label="Name" lazy-rules />
+            <q-input
+              type="email"
+              outlined
+              v-model="email"
+              label="Email"
+              lazy-rules
+            />
+
+            <q-input
+              type="password"
+              outlined
+              v-model="password"
+              label="Password"
+              lazy-rules
+            />
+
+            <div>
+              <q-btn
+                glossy
+                label="Register"
+                type="submit"
+                color="blue-7"
+                style="width: 100%"
+              />
+            </div>
+            <div class="q-mt-md">
+              <span> Already have account ? </span>
+              <router-link
+                to="/login"
+                style="text-decoration: none"
+                class="text-weight-medium text-blue-7"
+                >Login</router-link
+              >
+            </div>
+          </q-form>
+        </div>
+      </div>
+    </q-page>
   </q-page-container>
 </template>
+
+<script lang="ts">
+import { useQuasar } from 'quasar';
+
+export default {
+  setup() {
+    const $q = useQuasar();
+    const email = '';
+    const password = '';
+    const name = '';
+    return {
+      email,
+      password,
+      name,
+      $q,
+    };
+  },
+};
+</script>
